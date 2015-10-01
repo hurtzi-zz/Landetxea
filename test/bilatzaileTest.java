a7e13ff0956d7091c4dbd58e51a7ec1d4674dfa2
Urtzi Diaz Arberas <urtzidiaz@hotmail.com>
2015-10-01 13:44:09 +0200

    SearchTest1 add

--- test/bilatzaileTest.java
+++ test/bilatzaileTest.java
@@ -1,3 +1,8 @@
+
+import junit.framework.TestCase;
+import businessLogic.*;
+import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
+import java.rmi.RemoteException;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +13,47 @@
  *
  * @author Urtzi
  */
-public class bilatzaileTest {
-    
+public class bilatzaileTest extends TestCase{
+	public static ApplicationFacadeInterface facade;
+	private int a = 0;
+	
+	 public  bilatzaileTest(String testSearch) {
+	        super(testSearch);
+	    }
+         
+            @Override
+            //ejekututauko den testa baino lehen nahi duzuna (hasieratu bista kasu honetan)
+	    protected void setUp() throws Exception {
+	        super.setUp();
+	        // Creating Contact model
+	        facade = new FacadeImplementation();
+		// Creating Contact view
+	    
+	    }
+            
+            @Override
+	    protected void tearDown() throws Exception {
+		 if (a == 1)
+			 facade.close();
+	     super.tearDown();
+	    }
+            
+            //BILATZAILEAREN-aren proba kasuak
+	    public void testCreateGui1() throws InterruptedException {
+	    	try { //(1), (3), (5) eta (7) kasuak. Hau da, ONDO daudenak.
+	    		assertEquals(true, facade.SarchByCity("asd3@msn.com"));
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}   
+	    }
+	   
 }
+
+	 
+	 
+
+	 
+
+	 
